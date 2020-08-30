cdded4ad071acdb7c9db41e1082ac07c3cb36af8
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2020-08-30 19:14:15 +0530

    Update configure_jobs.groovy

--- configure_jobs.groovy
+++ configure_jobs.groovy
@@ -1,19 +1,35 @@
-pipeline { 
-    agent any 
-    stages {
-        stage('Build') { 
-            steps { 
-                echo "hello" 
-            }
+job('job1_pull_repo_build_image') {
+    steps {
+      scm {
+        git {
+          remote {
+            url('https://github.com/Adamaya/pipeline_implementation_with_k8s_-_jenkins.git')
+          }
+          branch('*/master')
         }
-        stage('Test'){
+      }
+      triggers {
+        scm('* * * * *')
+      }
+      build {
+            
             steps {
-                echo "test" 
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'make publish'
+                dockerBuilderPublisher {
+                  dockerFileDirectory("/home/php")
+                  fromRegistry {
+                    url("adamayasharma")
+                    credentialsId("3f885629-0783-4229-8808-f2610c781c80")
+                  }
+                cloud("docker")
+    
+                tagsString("adamayasharma/-gphp-webserver")
+                pushCredentialsId("3f885629-0783-4229-8808-f2610c781c80")
+                pushOnSuccess(true)
+                cleanImages(false)
+                cleanupWithJenkinsJobDelete(false)
+                noCache(false)
+                pull(true)
+                }    
             }
         }
     }
